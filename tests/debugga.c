93ece62f624221143dcd861c2f2ea9073d78e141
rofl0r <rofl0r@users.noreply.github.com>
2021-04-24 17:00:56 +0100

    improve demo debugger

--- tests/debugga.c
+++ tests/debugga.c
@@ -1,4 +1,5 @@
 #include "../debuglib.h"
+#include "../signals.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -38,9 +39,11 @@ int main(int argc, char** argv) {
 		puts("failed to launch debuggee");
 		return 1;
 	}
+	int last_command = 0;
 	while(1) {
 		nread = gets_nonblocking(0, linebuf, sizeof(linebuf));
 		if(nread) {
+	eval_cmd:;
 			switch(linebuf[0]) {
 				case 'b':
 					addr = strtol(&linebuf[2], NULL, 16);
@@ -49,21 +52,33 @@ int main(int argc, char** argv) {
 					break;
 				case 'c':
 					debugger_continue(&dbga, child);
+					last_command = linebuf[0];
 					break;
 				case 's':
 					debugger_single_step(&dbga, child);
+					last_command = linebuf[0];
 					break;
-
+				case '\n':
+					if(last_command) {
+						linebuf[0] = last_command;
+						goto eval_cmd;
+					}
+					/* fall-through */
 				default:
 					puts("unknown prefix");
 
 			}
+			last_command = linebuf[0];
 		}
 		if((de = debugger_get_events(&dbga, &child, &retval, 0)) != DE_NONE) {
 			printf("got a debugger event, retval %d, (%s)\n", retval, debugger_get_event_name(de));
-			if(de == DE_EXIT) {
+			switch(de) {
+			case DE_EXIT:
 				puts("child exited.");
 				return 0;
+			case DE_SIGNAL:
+				dprintf(2, "[%.5d] DE: %s [%d -> %s]\n", child, debugger_get_event_name(de), retval, get_signal_name(retval));
+				break;
 			}
 		}
 
